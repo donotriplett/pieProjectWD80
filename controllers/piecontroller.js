21bfed299d593537260b3eff4bf7dfc4886892f1
Donovan <donotriplett@gmail.com>
2021-04-06 16:44:54 -0400

    4/6/2021

--- controllers/piecontroller.js
+++ controllers/piecontroller.js
@@ -18,4 +18,83 @@ router.get("/", async (req, res) => {
     }
 })
 
+router.post("/", async (req, res) => {
+    const {
+        nameOfPie,
+        baseOfPie,
+        crust,
+        timeToBake,
+        servings,
+        rating
+    } = req.body
+
+    try {
+        const Pie = await PieModel.create({
+            nameOfPie,
+            baseOfPie,
+            crust,
+            timeToBake,
+            servings,
+            rating
+        })
+
+        res.status(201).json({
+            message: "Pie successfully created",
+            Pie
+        })
+    } catch(err) {
+        res.status(500).json({
+            message: `Failed to create pie: ${err}`
+        })
+    }
+})
+
+// findOne()
+router.get("/:nameOfPie", async (req, res) => {
+    try {
+      const locatedPie = await PieModel.findOne({
+        where: {
+          nameOfPie: req.params.nameOfPie,
+        },
+      });
+  
+      res.status(200).json({
+        message: "Pies successfully retrieved",
+        locatedPie,
+      });
+    } catch (err) {
+      res.status(500).json({
+        message: `Failed to retrieve pies: ${err}`,
+      });
+    }
+  });
+  
+  // update()
+  router.put("/:id", async (req, res) => {
+    const {
+      nameOfPie,
+      baseOfPie,
+      crust,
+      timeToBake,
+      servings,
+      rating,
+    } = req.body;
+  
+    try {
+      await PieModel.update(
+        { nameOfPie, baseOfPie, crust, timeToBake, servings, rating },
+        { where: { id: req.params.id }, returning: true }
+      ).then((result) => {
+        res.status(200).json({
+          message: "Pie successfully updated",
+          updatedPie: result,
+        });
+      });
+    } catch (err) {
+      res.status(500).json({
+        message: `Failed to update pie: ${err}`,
+      });
+    }
+  });
+
 module.exports = router
